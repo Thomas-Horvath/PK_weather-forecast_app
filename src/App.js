6474ea6e479063de69a7be49732122d985004a08
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-01-03 07:35:34 +0100

    create components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import VarosUrlap from'./Components/VarosUrlap/VarosUrlap';
 import Elorejelzes from './Components/Elorejelzes/Elorejelzes';
 
 export default function App() {
+
+function updateVaros(varos){
+alert('eredmény: ' + varos)
+}
+
   return (
     <div className="App">
        <h1>Időjárás előrejelzés</h1>
-      <VarosUrlap />
+      <VarosUrlap updateVaros={updateVaros}/>
       <Elorejelzes />
     </div>
   );
--- src/Components/VarosUrlap/VarosUrlap.jsx
+++ src/Components/VarosUrlap/VarosUrlap.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import './VarosUrlap.css';
 
-export default function VarosUrlap() {
-    return ( 
-    <form>
-        <label>
-            Város:
-             <input type="text" name='varos' />
-        </label>
-        <button type="submit">Lekérdez</button>
-    </form> )
+export default function VarosUrlap({ updateVaros }) {
+
+    const [varos, setVaros] = React.useState("");
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        updateVaros(varos)
+    }
+
+    const handleVarosChange = (event) => {
+        setVaros(event.target.value);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Város:
+                <input
+                    type="text"
+                    name='varos'
+
+                    value={varos}
+                    onChange={handleVarosChange} />
+            </label>
+            <button type="submit" >Lekérdez</button>
+        </form>)
 }
